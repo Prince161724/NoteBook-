e2c5a185b9bb45ff537673229d94e17657095304
Prince161724 <princekumarjobemail@gmail.com>
2025-07-20 13:40:53 +0530

    Merge remote-tracking branch 'origin/main'

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb+srv://princekumargiri50:Prince%40123@cluster0.xdlo603.mongodb.net/inotebook?retryWrites=true&w=majority&appName=Cluster0";
+
 
 const connecttoMongo = async () => {
     try {
